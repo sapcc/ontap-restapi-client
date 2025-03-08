2f9621354a717e9c5d238ee8baf9a0ed552f0464
Chuan Miao <chuan137@gmail.com>
2025-03-08 23:51:07 +0100

    update client

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"fmt"
 	"io"
+	"net/http"
 
 	"github.com/go-openapi/runtime"
 	httptransport "github.com/go-openapi/runtime/client"
@@ -11,34 +12,43 @@ import (
 )
 
 type NetAppClientConfig struct {
-	Host       string
-	ServerName string
-	Username   string
-	Password   string
-	Debug      bool
-	Insecure   bool
+	Host             string
+	Username         string
+	Password         string
+	Debug            bool
+	Insecure         bool
+	TargetServerName string
 }
 
-func NewClient(cfg NetAppClientConfig) (*apiclient.ONTAPRESTAPIOnlineReference, error) {
-	if cfg.ServerName == "" {
-		cfg.ServerName = cfg.Host
-	}
+type ONTAPAPI struct {
+	Client *apiclient.ONTAPRESTAPIOnlineReference
+}
 
+// CreateHTTPClient creates and configures an HTTP client with TLS settings.
+func CreateHTTPClient(insecure bool, targetServerName string) (*http.Client, error) {
 	tlsOptions := httptransport.TLSClientOptions{}
-	if cfg.Insecure {
+	if insecure {
 		tlsOptions.InsecureSkipVerify = true
-	} else if cfg.ServerName != "" {
-		tlsOptions.ServerName = cfg.ServerName
+	} else if targetServerName != "" {
+		tlsOptions.ServerName = targetServerName
 	}
-	tslClient, err := httptransport.TLSClient(tlsOptions)
+	client, err := httptransport.TLSClient(tlsOptions)
 	if err != nil {
 		return nil, err
 	}
+	return client, nil
+}
 
-	// data is of type *models.ErrorResponse
-	// maybe we need to manually parse error in some other consumer?
+func NewClient(cfg NetAppClientConfig, client *http.Client) (*ONTAPAPI, error) {
+	var err error
+	if client == nil {
+		client, err = CreateHTTPClient(cfg.Insecure, cfg.TargetServerName)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-	rt := httptransport.NewWithClient(cfg.Host, apiclient.DefaultBasePath, apiclient.DefaultSchemes, tslClient)
+	rt := httptransport.NewWithClient(cfg.Host, apiclient.DefaultBasePath, apiclient.DefaultSchemes, client)
 	rt.Debug = cfg.Debug
 	rt.DefaultAuthentication = cfg.BasicAuth()
 	rt.Consumers["application/hal+json"] = runtime.JSONConsumer()
@@ -49,7 +59,7 @@ func NewClient(cfg NetAppClientConfig) (*apiclient.ONTAPRESTAPIOnlineReference, 
 			return err
 		}
 	})
-	return apiclient.New(rt, strfmt.Default), nil
+	return &ONTAPAPI{Client: apiclient.New(rt, strfmt.Default)}, nil
 }
 
 func (c NetAppClientConfig) BasicAuth() runtime.ClientAuthInfoWriter {
